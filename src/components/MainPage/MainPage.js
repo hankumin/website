a7ab530358a1a59386523979c045720e16af8d78
Calvin Han <hankumin@gmail.com>
2022-11-11 00:34:15 -0500

    Added more text and styling

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -12,7 +12,6 @@ export function MainPage(){
                     <div className={styles.main_title_Left}>
                         <h1>Welcome</h1>
                         <p className={`${styles.text}`}>
-                        
                             Hello! Welcome to my website. My name is Calvin Han, a fullstack developer who is 
                             currently developing more on his frontend skills. If you Have any comments please feel
                             free to message me!
@@ -22,7 +21,17 @@ export function MainPage(){
                     <img className={`${styles.image}`} src={photoOfCalvin} alt='CalvinHan'/>
                 </div>
                 <div id='section2' className={`${styles.main_flex2}`}>
-                    yes
+                    <h1>Skills</h1>
+                    {/* <p className={`${styles.text}`}>
+                        Languages:  JavaScript, Java, C++, PHP, HTML, OCaml, C, CSS, LaTeX
+                        Frameworks: Java Spring Boot, Reactjs, Angular
+                        Software:   Jira, Confluence, Vim, Eclipse, Microsoft Office, Visual Studio, Atom
+                    </p> */}
+                    <ul className={`${styles.list}`}>
+                        <li>Languages:  JavaScript, Java, C++, PHP, HTML, OCaml, C, CSS, LaTeX</li>
+                        <li>Frameworks: Java Spring Boot, Reactjs, Angular</li>
+                        <li>Software:   Jira, Confluence, Vim, Eclipse, Microsoft Office, Visual Studio, Atom</li>
+                    </ul>
                 </div>
                 <div id='section2' className={`${styles.main_flex1}`}>
                     yes
